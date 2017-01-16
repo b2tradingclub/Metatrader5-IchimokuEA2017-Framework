f9a00145b86b68a1799895ea88b68133ebbcb34d
DVX opensource projects <reunisoft@gmail.com>
2017-01-16 21:55:24 +0100

    Update IchimokuScanner.php
    Has been removed : All processing about table "notification". (no need for this table anymore)

--- IchimokuScanner.php
+++ IchimokuScanner.php
@@ -3,7 +3,6 @@ define("MYSQL_SERVER", "localhost");
 define("MYSQL_USER", "id517966_ichimoku");
 define("MYSQL_PASSWORD", "");
 define("MYSQL_DB", "id517966_ichimoku");
-define("MAIN_TABLE_NAME","notification");
 define("CREATE_DB_IF_NOT_EXISTS", true);
 define("CREATE_TABLES_IF_NOT_EXIST", true);
 define("LOG_IP", true);
@@ -89,20 +88,11 @@ if (isset($_GET['reset_all'])) {
     if ($db->connect_errno) {
         exit;
     }
-    $r = mysqli_query($db, "delete from notification");
     $r = mysqli_query($db, "delete from ssb_alert");
     $db->close();
     exit;
 }
-if (isset($_GET['reset_notifications'])) {
-    $db = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
-    if ($db->connect_errno) {
-        exit;
-    }
-    $r = mysqli_query($db, "delete from notification");
-    $db->close();
-    exit;
-}
+
 if (isset($_GET['reset_ssb_alerts'])) {
     $db = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
     if ($db->connect_errno) {
@@ -116,26 +106,6 @@ echo "<html><head><style> table, th, td { border: 1px solid black; border-collap
 echo "<img src='ichimokuscannerlogo.PNG'>";
 echo "<h2>Ichimoku Scanner</h2>";
 echo "<h3>Experimental version</h3><a href='http://traderetgagner.blogspot.com'>traderetgagner.blogspot.com</a><br/>";
-if (isset($_GET['notification'])) {
-    $notification = $_GET['notification'];
-    echo "received=  [[$notification]]<br/>";
-    $db = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
-    if ($db->connect_errno) {
-        echo "Error : " . $db->connect_errno . " <br/>";
-        exit;
-    }
-    $r = mysqli_query($db, "select * from notification");
-    if ($r->num_rows == 0){
-        echo 'No notifications.<br/>';
-    } else {
-        echo $r->num_rows . " notifications in table<br/>";
-    }
-    $timestamp = (new DateTime())->format('Y-m-d H:i:s');
-    $r = mysqli_query($db, "insert into " . MAIN_TABLE_NAME . " (timestamp, message) values ('" . $timestamp . "', '" . $notification . "')");
-    if (DEBUG) echo 'Notification recorded OK.<br/>';
-    $db->close();
-    exit;
-}
 if (isset($_GET['upload_ssb_alert'])) {
     $ssbalert = $_GET['upload_ssb_alert'];
     echo "received=  [[$ssbalert]]<br/>";
@@ -250,42 +220,6 @@ foreach($arrayname as $name){
         }
     }
 }
-/*
-echo '<br/>';
-echo '<br/>';
-$db = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
-if ($db->connect_errno) {
-echo "Error : " . $db->connect_errno . " <br/>";
-exit;
-}
-$r = mysqli_query($db, "select * from notification");
-if ($r->num_rows == 0){
-echo 'No notifications.<br/>';
-} else {
-echo $r->num_rows . " notifications in table<br/>";
-$filter = "";
-if (isset($_GET['filter'])) {
-$filter = $_GET['filter'];
-}
-if (trim($filter) == ""){
-$r = mysqli_query($db, "select * from " . MAIN_TABLE_NAME . " order by timestamp desc");
-} else {
-$r = mysqli_query($db, "select * from " . MAIN_TABLE_NAME . " where message like '%" . $filter . "%' order by timestamp desc");
-}
-echo '<br/>';
-echo 'List of notifications<br/>';
-if ($r->num_rows > 0) {
-while($row = $r->fetch_assoc()) {
-echo '<br/>';
-//echo $row["timestamp"] . ' : ' .  $row["message"] . '<br/>';
-echo $row["message"] . '<br/>';
-}
-} else {
-//echo "0 results";
-}
-$db->close();
-}
-*/
 $db->close();
 echo "</body></html>";
 ?>
